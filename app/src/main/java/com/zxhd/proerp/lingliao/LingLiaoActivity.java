b82e56a04c378d9d66e27cdc714ee6618183e3aa
chenjing666 <1774383254@qq.com>
2019-03-29 14:24:22 +0800

    0329

--- app/src/main/java/com/zxhd/proerp/lingliao/LingLiaoActivity.java
+++ app/src/main/java/com/zxhd/proerp/lingliao/LingLiaoActivity.java
@@ -2,22 +2,24 @@ package com.zxhd.proerp.lingliao;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.wyt.searchbox.SearchFragment;
+import com.wyt.searchbox.custom.IOnSearchClickListener;
 import com.zxhd.proerp.R;
 import com.zxhd.proerp.cont.Api;
 import com.zxhd.proerp.outware.OutWareList;
-import com.zxhd.proerp.outware.WareOutAdapter;
 import com.zxhd.proerp.outware.outwaredetails.WareOutDetailsActivity;
 import com.zxhd.proerp.utils.http.CallBackUtil;
 import com.zxhd.proerp.utils.http.OkhttpUtil;
@@ -30,33 +32,45 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import okhttp3.Call;
 
 public class LingLiaoActivity extends AppCompatActivity {
+    @BindView(R.id.back)
+    TextView back;
+    @BindView(R.id.search_ware_out_lingliao)
+    TextView searchWareOutLingliao;
     private RecyclerView mRecyclerView;
     private List<OutWareList> mList = new ArrayList<OutWareList>();
-    private WareOutAdapter wareOutAdapter;
+    private LingLiaoListAdapter wareOutAdapter;
     private ProgressBar progressBar;
     private int pageSize = 20;
     private int nowPage = 1;
+    private String searchNum = "";
+    SearchFragment searchFragment = SearchFragment.newInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ling_liao);
+        ButterKnife.bind(this);
         progressBar = findViewById(R.id.loading);
         mRecyclerView = findViewById(R.id.recyclerView_ware_out);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        wareOutAdapter = new WareOutAdapter(this);
+        wareOutAdapter = new LingLiaoListAdapter(this);
         wareOutAdapter.setOnItemClickListener(listener);
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
         mRecyclerView.setAdapter(wareOutAdapter);
 
-        //返回
-        findViewById(R.id.back).setOnClickListener(new View.OnClickListener() {
+        searchFragment.setOnSearchClickListener(new IOnSearchClickListener() {
             @Override
-            public void onClick(View view) {
-                finish();
+            public void OnSearchClick(String keyword) {
+                //这里处理逻辑
+                searchNum = keyword;
+                //输入或扫描出库条码，请求出库单详情，跳转到详情页
+                getMList(keyword);
             }
         });
     }
@@ -78,10 +92,10 @@ public class LingLiaoActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        getMList();
+        getMList("");
     }
 
-    private WareOutAdapter.OnItemClickListener listener = new WareOutAdapter.OnItemClickListener() {
+    private LingLiaoListAdapter.OnItemClickListener listener = new LingLiaoListAdapter.OnItemClickListener() {
         @Override
         public void onItemClick(View view, int position) {
             Intent intent = new Intent(LingLiaoActivity.this, WareOutDetailsActivity.class);
@@ -96,23 +110,30 @@ public class LingLiaoActivity extends AppCompatActivity {
     /**
      * 获取出库需求单列表
      */
-    private void getMList() {
+    private void getMList(String searchText) {
         if (mList != null) {
             mList.clear();
         }
         HashMap<String, String> paramsMap = new HashMap<>();
-        paramsMap.put("offSet", nowPage + "");
-        paramsMap.put("pageSize", pageSize + "");
+        if (searchText.isEmpty()) {
+            paramsMap.put("offSet", nowPage + "");
+            paramsMap.put("pageSize", pageSize + "");
+        } else {
+            paramsMap.put("search", searchText);
+            paramsMap.put("state", "");
+            paramsMap.put("offSet", nowPage + "");
+            paramsMap.put("pageSize", pageSize + "");
+        }
         progressBar.setVisibility(View.VISIBLE);
         OkhttpUtil.okHttpPost(Api.OUT_WARE_LIST_LINGLIAO, paramsMap, new CallBackUtil.CallBackString() {
             @Override
             public void onFailure(Call call, Exception e) {
+                progressBar.setVisibility(View.GONE);
                 Log.e("onFailure", e.toString());
             }
 
             @Override
             public void onResponse(String response) {
-//                Log.e("onResponse", response);
                 progressBar.setVisibility(View.GONE);
                 try {
                     JSONObject object = new JSONObject(response);
@@ -148,5 +169,17 @@ public class LingLiaoActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @OnClick({R.id.back, R.id.search_ware_out_lingliao})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.back:
+                finish();
+                break;
+            case R.id.search_ware_out_lingliao:
+                searchFragment.showFragment(getSupportFragmentManager(), SearchFragment.TAG);
+                break;
+        }
     }
 }
--- app/src/main/java/com/zxhd/proerp/lingliao/LingLiaoListAdapter.java
+++ app/src/main/java/com/zxhd/proerp/lingliao/LingLiaoListAdapter.java
@@ -6,6 +6,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.zxhd.proerp.R;
 import com.zxhd.proerp.outware.OutWareList;
@@ -15,6 +17,7 @@ import java.util.List;
 public class LingLiaoListAdapter extends RecyclerView.Adapter<LingLiaoListAdapter.ListViewHolder> {
     private List<OutWareList> mList;
     private Context context;
+    private OutWareList mData;
 
     public LingLiaoListAdapter(Context context) {
         this.context = context;
@@ -33,8 +36,37 @@ public class LingLiaoListAdapter extends RecyclerView.Adapter<LingLiaoListAdapte
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ListViewHolder listViewHolder, int i) {
+    public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
+        mData = mList.get(position);
+        holder.ll_num.setText(mData.getLl_num_lingliao());
+        holder.ll_ware.setText(mData.getLl_ware());
+        holder.ll_user_create.setText(mData.getLl_user_create());
+        holder.ll_time.setText(mData.getLl_time());
+        holder.ll_remark.setText(mData.getLl_remark());
+        int status = mData.getLl_status();
+        switch (status) {
+            case 0:
+                holder.ll_status.setText("未出库");
+                holder.ll_status.setTextColor(context.getResources().getColor(R.color.red));
+                break;
+            case 1:
+                holder.ll_status.setText("部分出库");
+                holder.ll_status.setTextColor(context.getResources().getColor(R.color.green));
+                break;
+            case 2:
+                holder.ll_status.setText("全部出库");
+                break;
+        }
 
+        //item点击事件
+        if (onItemClickListener != null) {
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onItemClickListener.onItemClick(holder.itemView, position);
+                }
+            });
+        }
     }
 
     @Override
@@ -43,10 +75,22 @@ public class LingLiaoListAdapter extends RecyclerView.Adapter<LingLiaoListAdapte
     }
 
     class ListViewHolder extends RecyclerView.ViewHolder {
-        public ListViewHolder(View itemView) {
+        private LinearLayout linearLayout;
+        private TextView ll_num, ll_ware, ll_status, ll_user_create, ll_time, ll_remark;
+
+        public ListViewHolder(@NonNull View itemView) {
             super(itemView);
+            linearLayout = itemView.findViewById(R.id.ll_ware_out);
+            ll_num = itemView.findViewById(R.id.ll_num);
+            ll_ware = itemView.findViewById(R.id.ll_ware);
+            ll_status = itemView.findViewById(R.id.ll_status);
+//            ll_type = itemView.findViewById(R.id.ll_type);
+            ll_user_create = itemView.findViewById(R.id.ll_user_create);
+            ll_time = itemView.findViewById(R.id.ll_time);
+            ll_remark = itemView.findViewById(R.id.ll_remark);
         }
     }
+
     // ###################################   item的点击事件（接口回调） ##############
     public interface OnItemClickListener {
 
